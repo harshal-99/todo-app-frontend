11b2fd3df5704a9e147bd4b188bc653df84cfa06
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2022-08-10 15:53:10 +0530

    Implement SnackBar

---
+++ src/components/SnackBar.jsx
@@ -0,0 +1,28 @@
+import {Alert, Button, Snackbar, Stack} from "@mui/material";
+
+import {useState} from "react";
+
+
+const SnackBar = ({message}) => {
+	const [open, setOpen] = useState(true)
+
+	const handleClose = (event, reason) => {
+		if (reason === 'clickaway') {
+			return
+		}
+		setOpen(false)
+	}
+
+	return (
+		<Snackbar
+			anchorOrigin={{vertical: 'bottom', horizontal: 'center'}}
+			open={open}
+			onClose={handleClose}
+			autoHideDuration={3000}
+		>
+			<Alert onClose={handleClose} severity="error">{message}</Alert>
+		</Snackbar>
+	)
+}
+
+export default SnackBar
