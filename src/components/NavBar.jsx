6935c41f8d29e26f8d1561bc6ed0b1d6bc33b0a5
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2022-08-10 16:15:43 +0530

    Fix styles

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,13 +7,13 @@ const NavBar = () => {
 	const isLoggedIn = auth?.user
 		? <Button variant="outlined" style={{color: "white"}} color="error" onClick={() => auth.logout()}>Logout</Button>
 		: <>
-			<Button variant="outlined"><Link to='/login'>Login</Link></Button>
-			<Button variant="outlined"><Link to='/signup'>Signup</Link></Button>
+			<Button variant="outlined"><Link style={{color: 'white'}} to='/login'>Login</Link></Button>
+			<Button variant="outlined"><Link style={{color: 'white'}} to='/signup'>Signup</Link></Button>
 		</>
 	return (
 		<AppBar position="static">
 			<Container maxWidth="x1">
-				<Box sx={{flexGrow: 1, display: {xs: "flex", md: "none", justifyContent: "flex-end"}}}>
+				<Box>
 					{isLoggedIn}
 				</Box>
 			</Container>
